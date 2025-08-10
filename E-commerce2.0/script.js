200b712e6d387b235b7ccd994439bbb636b514a2
SarfarazElahie <sarfarazelahie62@gmail.com>
2025-08-10 09:58:42 +0530

    Add files via upload

---
+++ E-commerce2.0/script.js
@@ -0,0 +1,19 @@
+
+// Hamberger Menu 
+let bar = document.querySelector("#bar")
+let nav = document.querySelector("#navbar")
+let close = document.querySelector("#close")
+
+if(bar){
+    bar.addEventListener("click", ()=>{
+        nav.classList.add("active")
+    })
+}
+
+if(close){
+
+        close.addEventListener("click", ()=>{
+        nav.classList.remove("active")
+    })
+    
+}
